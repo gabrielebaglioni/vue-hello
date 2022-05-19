6d2ef8eadcee263053a422e5f0bbacfaa20e045f
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-19 13:37:16 +0200

    img implementata

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = new Vue({
     el: "#app",
     data: {
-        message: "HelloWorld"
+        message: "HelloWorld",
+        img: "https://www.laramind.com/blog/wp-content/uploads/2019/05/1_nq9cdMxtdhQ0ZGL8OuSCUQ.jpg"
     }
 })
